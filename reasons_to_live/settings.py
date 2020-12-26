8c7b686069a8ebfff470f954157acbe468d25603
advaithm <advaith.madhukar@gmail.com>
2020-12-26 22:54:54 +0530

    restructred settings.py to make it simpler to run local insatances

--- reasons_to_live/settings.py
+++ reasons_to_live/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("DEBUG") == True:
+if os.environ.get("DEBUG_FLAG"):
+    SECRET_KEY = "this_shitty_key_for_testing1314t245t"
     DEBUG = True
 else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
