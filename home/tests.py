8094e552ff4d27a2fbd7096bd1bf536885846f3b
advaithm <advaith.madhukar@gmail.com>
2020-12-28 16:02:06 +0530

    working on tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from home.models import reason_unapproved, reason_approved
 
 # Create your tests here.
+
+
+class Reasons_upapproved_test(TestCase):
+    def test(self):
+        reason_unapproved.objects.create(
+            reason="a simple unapproved reason",
+            email="email@email,com",
+            display_name="null",
+        )
+
+
+class reasons_approved_test(TestCase):
+    def test(self):
+        reason_approved.objects.create(
+            reason="a simple approved reason",
+            email="email@email,com",
+            display_name="null",
+        )
